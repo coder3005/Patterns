b3140251b490b0b23c4f49042e53535e9714e1c8
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-26 00:03:05 +0530

    Create invertedstar.java

---
+++ invertedstar.java
@@ -0,0 +1,12 @@
+package Patterns;
+
+public class invertedstar {
+    public static void main(String[] args) {
+        for(int line=4;line>=1;line--) {
+            for(int star=1;star<=line;star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
