9d63ae23425b2a16272e9213dc7b1c652648091c
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-26 00:01:09 +0530

    Create character.java

---
+++ character.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class character {
+    public static void main(String[] args) {
+        char ch='A';
+        for(int line=1;line<=4;line++) {
+            for(int chars=1;chars<=line;chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
